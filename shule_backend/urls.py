4555ae2c5d2b47375d15b634d1b6bd9cae414498
Peter Njenga <peternjengask@gmail.com>
2025-02-07 10:00:54 +0300

    JWT authentication

--- shule_backend/urls.py
+++ shule_backend/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('projects/', include('projects.urls'))
 ]
